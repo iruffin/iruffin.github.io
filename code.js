ca186e7b61ecece42f180f33ea2d1281af96adff
Iverson <orlandopeaches@gmail.com>
2024-01-21 06:04:30 -0800

    my website upload

---
+++ code.js
@@ -0,0 +1,31 @@
+function $(x){
+    return document.getElementById(x);
+}
+
+// Your JavaScript code goes here
+document.addEventListener("DOMContentLoaded", function () {
+
+    // This function will be executed when the DOM is fully loaded
+   
+    console.log("heylo");
+
+  
+});
+
+//redirection to different page functions
+
+function redirectToAboutPage() {
+    // Change 'linked_page.html' to the actual filename or URL of your linked page
+    window.location.href = 'about.html';
+}
+
+function redirectToWorkPage() {
+    // Change 'linked_page.html' to the actual filename or URL of your linked page
+    window.location.href = 'work.html';
+}
+
+function redirectToHomePage() {
+    // Change 'linked_page.html' to the actual filename or URL of your linked page
+    window.location.href = 'home.html';
+}
+
